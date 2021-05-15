f2431071cc62472ee04f5896c8dc4c898e68c92f
Nathan McKenzie <nathantaylormckenzie@hotmail.com>
2021-05-14 18:16:59 -0700

    CSS Improvements

--- client/src/customer-components/RestaurantPage.js
+++ client/src/customer-components/RestaurantPage.js
@@ -1,18 +1,44 @@
 import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar";
+import MenuItemModal from "./MenuItemModal";
 
 export default function RestaurantPage({ restaurant, setRestaurant }) {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className="res">
       <NavBar />
       <div className="restaurant-page">
         <div className="restaurant-page-left-side">
-          {console.log("1:51", restaurant)}
           <div className="restaurant-page-restaurant-title">
             {restaurant.name}
             <div className="test-scroll">.</div>
           </div>
-          <div className="menu">Menu</div>
+          <div className="menu">
+            Menu{" "}
+            <div className="menu-list">
+              {restaurant.menuItems.map((menuItem) => {
+                return (
+                  <div
+                    className="menu-item-container"
+                    onClick={() => setShowModal(!showModal)}
+                  >
+                    {menuItem.name}
+                    <br />
+                    {menuItem.description}
+                    <br />
+                    <div className="menu-price">CA${menuItem.price}</div>
+                    {showModal ? (
+                      <MenuItemModal
+                        name={menuItem.name}
+                        price={menuItem.price}
+                      />
+                    ) : null}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
         <div className="restaurant-page-right-side">hi</div>
       </div>
